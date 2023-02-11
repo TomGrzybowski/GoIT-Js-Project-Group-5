0bd4b6753580b179b13995ffa954a580e66ed1a6
SebastianSzczypiorowski <110854817+SebastianSzczypiorowski@users.noreply.github.com>
2023-02-11 12:15:47 +0100

    1

---
+++ src/js/search-movies.js
@@ -0,0 +1,16 @@
+async function searchMovies(keyword) {
+    const apiKey = "7bfaca5914dfe808eee9ce7ecac1ff40";
+    const url =`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${keyword}`;
+
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        return data.results;
+    } catch (error) {
+        console.log(error);
+    }
+}
+let keyword = "Kungfu Panda"
+searchMovies(keyword).then(movies => {
+    console.log(movies);
+});
