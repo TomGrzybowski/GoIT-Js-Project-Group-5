d4f1115af2b07f57599dde526e4d2d0cd799cc93
Jakub Antoniak <antonipomorzanin@gmail.com>
2023-02-11 17:50:58 +0100

    11.02

--- src/js/main.js
+++ src/js/main.js
@@ -1,82 +1,87 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const movieImage = document.querySelector('.movie-card__image');
+// const moviesList = document.querySelector('.movies__list');
+// const movieImage = document.querySelector('.movie-card__image');
 
-const importQueue = document.querySelector('.btn-import');
-const importQueueBox = document.querySelector('.import');
+// const importQueue = document.querySelector('.btn-import');
+// const importQueueBox = document.querySelector('.import');
 
-const params = {
-  KEY: `4e7929e7857820c6b400adab328687af`, //API KEY
-  PAGE: 1,
-  LANGUAGE: 'en-US',
-};
+// const params = {
+//   KEY: `4e7929e7857820c6b400adab328687af`, //API KEY
+//   PAGE: 1,
+//   LANGUAGE: 'en-US',
+// };
 
-const { KEY, PAGE, LANGUAGE } = params;
-//API DOKUMENTACJA     https://developers.themoviedb.org/3/search/search-keywords
+// const { KEY, PAGE, LANGUAGE } = params;
+// //API DOKUMENTACJA     https://developers.themoviedb.org/3/search/search-keywords
 
-async function fetchMovies() {
-  try {
-    const url = new URL('https://api.themoviedb.org/3/movie/popular');
-    url.searchParams.set('api_key', KEY);
-    url.searchParams.set('page', PAGE);
-    url.searchParams.set('language', LANGUAGE);
+// async function fetchMovies() {
+//   try {
+//     const url = new URL('https://api.themoviedb.org/3/movie/popular');
+//     url.searchParams.set('api_key', KEY);
+//     url.searchParams.set('page', PAGE);
+//     url.searchParams.set('language', LANGUAGE);
 
-    const response = await axios.get(url);
-    return response;
-  } catch (error) {
-    console.log('ERROR FETCH', error);
-    return error;
-  }
-}
-function build(result, parent) {
-  result.forEach(el => {
-    const image = document.createElement('img');
-    const img = `https://image.tmdb.org/t/p/w500${result.backdrop_path}`;
-    image.setAttribute('src', img);
-    movieImage.appendChild(image);
-    image.classList.add('image');
+//     const response = await axios.get(url);
+//     return response;
+//   } catch (error) {
+//     console.log('ERROR FETCH', error);
+//     return error;
+//   }
+// }
 
-    const title = document.createElement('p');
-    title.textContent = result.title;
-    movieImage.appendChild(title);
-  });
+// function build(result, parent) {
+//   moviesList.innerHTML = '';
 
-  //   const button = document.createElement('button');
-  //   button.classList.add('button');
-  //   button.textContent = 'Click me';
-  //   movieCard.appendChild(button);
-  //   button.addEventListener('click', () => {
-  //     queueExport(result);
-  //   });
-}
+//   const image = document.createElement('img');
+//   const img = `https://image.tmdb.org/t/p/w300${result.backdrop_path}`;
+//   image.setAttribute('src', img);
+//   image.classList.add('image');
+//   movieImage.appendChild(image);
+//   const title = document.createElement('p');
+//   title.textContent = result.title;
+//   movieImage.appendChild(title);
+//   // const button = document.createElement('button');
+//   // button.classList.add('button');
+//   // button.textContent = 'Click me';
+//   // movieImage.appendChild(button);
+//   // button.addEventListener('click', () => {
+//   //   queueExport(result);
+//   // });
+// }
 
-const queue = [];
-function queueExport(result) {
-  queue.push(result.title);
-  window.localStorage.setItem('Queue', JSON.stringify(queue));
-}
+// function buildCard() {
+//   const newCard = createElement('li');
+//   newCard.classList.add('movie-card');
+// }
 
-fetchMovies().then(function (response) {
-  const moviesresults = response.data.results;
-  console.log('ARRAY OF MOVIE RESULTS', moviesresults);
-  moviesresults.forEach(result => {
-    build(result, movieImage);
-  });
-});
+// const queue = [];
+// function queueExport(result) {
+//   queue.push(result.title);
+//   window.localStorage.setItem('Queue', JSON.stringify(queue));
+// }
 
-importQueue.addEventListener('click', () => {
-  const getItems = window.localStorage.getItem('Queue');
-  const parsed = JSON.parse(getItems);
+// fetchMovies().then(function (response) {
+//   const moviesresults = response.data.results;
+//   console.log('ARRAY OF MOVIE RESULTS', moviesresults);
+//   moviesresults.forEach(result => {
+//     build(result, movieImage);
+//   });
+// });
 
-  fetchMovies().then(function (items) {
-    const results = items.data.results;
+// importQueue.addEventListener('click', () => {
+//   const getItems = window.localStorage.getItem('Queue');
+//   const parsed = JSON.parse(getItems);
 
-    parsed.forEach(parsedItem => {
-      results.filter(movie => {
-        if (movie.title === parsedItem) {
-          build(movie, importQueueBox);
-        }
-      });
-    });
-  });
-});
+//   fetchMovies().then(function (items) {
+//     const results = items.data.results;
+
+//     parsed.forEach(parsedItem => {
+//       results.filter(movie => {
+//         if (movie.title === parsedItem) {
+//           build(movie, importQueueBox);
+//         }
+//       });
+//     });
+//   });
+// });
