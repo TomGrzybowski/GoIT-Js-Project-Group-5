36f0410f0b5aad4b6c3020e42b1b8ebee9d54e55
TomekGrzybek <t.grzybowski94@gmail.com>
2023-02-12 14:51:47 +0100

    loading trending movies

---
+++ src/js/createCards.js
@@ -0,0 +1,36 @@
+export default function createMovieCard({
+  title,
+  image,
+  genres,
+  year,
+  rating,
+}) {
+  const LI = document.createElement('li');
+  LI.classList.add('movie-card');
+
+  const IMAGE_DIV = document.createElement('div');
+  IMAGE_DIV.classList.add('movie-card__image');
+  const IMG_HTML = `<img class="image" src="${image}" alt="${title} poster">`;
+  IMAGE_DIV.insertAdjacentHTML('beforeend', IMG_HTML);
+
+  const DETAILS = document.createElement('div');
+  DETAILS.classList.add('movie-card__details');
+
+  const TITLE = document.createElement('p');
+  TITLE.classList.add('movie-title');
+  TITLE.innerText = title.toUpperCase();
+
+  const GENRE = document.createElement('p');
+  GENRE.classList.add('movie-genre');
+  GENRE.innerText = `${genres} | ${year}`;
+
+  DETAILS.insertAdjacentElement('beforeend', TITLE);
+  DETAILS.insertAdjacentElement('beforeend', GENRE);
+
+  LI.insertAdjacentElement('beforeend', IMAGE_DIV);
+  LI.insertAdjacentElement('beforeend', DETAILS);
+
+  //   return LI;
+  const LIST = document.querySelector('.movies__list');
+  LIST.insertAdjacentElement('beforeend', LI);
+}
