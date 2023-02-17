40e0f0d8215b2f092ed5d20f054ca515c5bab288
Jakub Antoniak <antonipomorzanin@gmail.com>
2023-02-17 17:05:48 +0100

    17.02

--- src/js/initial-fetch.js
+++ src/js/initial-fetch.js
@@ -33,6 +33,19 @@ async function getMovieDetails(movieId) {
   return { title, image, genres: genresString, year, rating };
 }
 
+function createPagination() {
+  const paginationDiv = document.querySelector('.movies__pagination');
+
+  const paginationButton = document.createElement('button');
+  paginationButton.classList.add('load-more-button');
+  const arrowButton = `<button class="load-more-button"><svg class="main__svg" width="16" heigth="16">
+  <use href="./icons/icons.svg#arrow"></use>
+</svg></button>`;
+
+  paginationDiv.insertAdjacentHTML('beforeend', arrowButton);
+  paginationDiv.insertAdjacentElement('beforeend', paginationButton);
+}
+
 export async function getAndDisplayTrendingMovies(page = 1) {
   loading();
   try {
@@ -51,10 +64,12 @@ export async function getAndDisplayTrendingMovies(page = 1) {
     };
 
     console.log(paginationData);
-    // Kuba, tutaj wpisz funkcję createPagination({currentPage, amountOfPages})
+    createPagination();
   } catch (error) {
     console.log(error.message);
   }
 
   Loading.remove();
 }
+
+// { currentPage, amountOfPages }
