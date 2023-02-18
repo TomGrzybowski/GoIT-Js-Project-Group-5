7a6201051e438b7097751a6cbfbd08baba80c837
SebastianSzczypiorowski <110854817+SebastianSzczypiorowski@users.noreply.github.com>
2023-02-18 19:58:06 +0100

    1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './sass/main.scss';
 import { getAndDisplayTrendingMovies } from './js/initial-fetch.js';
 import './js/search-movies.js';
-// window.onload = getAndDisplayTrendingMovies(1);
+window.onload = getAndDisplayTrendingMovies(1);
