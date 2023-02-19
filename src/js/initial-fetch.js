de1f742f32b4ad605fc2d37f47e5d7eb9d588192
TomekGrzybek <t.grzybowski94@gmail.com>
2023-02-19 15:25:24 +0100

    pagination setup

--- src/js/initial-fetch.js
+++ src/js/initial-fetch.js
@@ -1,6 +1,7 @@
 import createMovieCard from './createCards';
 import loading from './loading';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { createPagination } from './pagination';
 
 const API_KEY = '7bfaca5914dfe808eee9ce7ecac1ff40';
 
@@ -48,7 +49,7 @@ export async function getAndDisplayTrendingMovies(page = 1) {
     }
     const paginationData = {
       currentPage: trendingMovies.page,
-      amountOfPages: trendingMovies.total_pages,
+      totalPages: trendingMovies.total_pages,
     };
 
     createPagination(paginationData);
@@ -58,77 +59,3 @@ export async function getAndDisplayTrendingMovies(page = 1) {
 
   Loading.remove();
 }
-
-//
-//
-
-//
-//
-
-//
-//
-function createPagination({ currentPage, amountOfPages }) {
-  const nextButton = document.querySelector('#next-button');
-  const prevButton = document.querySelector('#prev-button');
-  const paginationNumbers = document.querySelector('.pagination-numbers');
-  const prevDots = document.querySelector('#prev-dots');
-  const nextDots = document.querySelector('#next-dots');
-  const currentPageButton = document.querySelector(
-    `.pagination-button-${currentPage}`
-  );
-
-  function paginationButtonCreation(currentPage) {
-    const paginationButton = document.createElement('button');
-    paginationButton.classList.add('pagination-button');
-    paginationButton.classList.add(`pagination-button-${currentPage}`);
-    paginationButton.innerHTML = currentPage;
-
-    paginationNumbers.insertAdjacentElement('beforeend', paginationButton);
-  }
-
-  const getPaginationButtons = () => {
-    for (let i = 2; i <= 6; i++) {
-      paginationButtonCreation(i);
-      const selectedButton = document.querySelector(`.pagination-button-${i}`);
-
-      selectedButton.textContent = currentPage + (i - 2);
-
-      if (currentPage === currentPage + (i - 2)) {
-        selectedButton.classList.add('active');
-      }
-      const firstButton = document.querySelector('.pagination-button-1');
-      const lastButton = document.querySelector('.pagination-button-7');
-      firstButton.classList.add('hidden');
-      lastButton.textContent = amountOfPages;
-    }
-
-    if (currentPage === 1) {
-      prevButton.classList.add('hidden');
-      prevDots.classList.add('hidden');
-    }
-    if (currentPage === amountOfPages) {
-      nextButton.classList.add('hidden');
-      nextDots.classList.add('hidden');
-    }
-  };
-  getPaginationButtons();
-
-  // function dotsAndArrowsRemoval(currentPage, amountOfPages) {}
-
-  const button = document.querySelector('.pagination-button');
-  button.addEventListener('click', handleActivePageNumber(currentPage));
-  console.log(currentPage);
-}
-
-//
-
-//
-
-//
-//
-//
-//
-//
-//
-//
-//
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,1 +1,223 @@
+// export function createPagination({ currentPage, amountOfPages }) {
+//   const nextButton = document.querySelector('#next-button');
+//   const prevButton = document.querySelector('#prev-button');
+//   const paginationNumbers = document.querySelector('.pagination-numbers');
+//   const prevDots = document.querySelector('#prev-dots');
+//   const nextDots = document.querySelector('#next-dots');
+//   const currentPageButton = document.querySelector(
+//     `.pagination-button-${currentPage}`
+//   );
 
+//   //   function paginationButtonCreation(currentPage) {
+//   //     const paginationButton = document.createElement('button');
+//   //     paginationButton.classList.add('pagination-button');
+//   //     paginationButton.classList.add(`pagination-button-${currentPage}`);
+//   //     paginationButton.innerHTML = currentPage;
+
+//   //     paginationNumbers.insertAdjacentElement('beforeend', paginationButton);
+//   //   }
+
+//   const getPaginationButtons = () => {
+//     // for (let i = 2; i <= 6; i++) {
+//     //   //   paginationButtonCreation(i);
+//     //   const selectedButton = document.querySelector(`.pagination-button-${i}`);
+//     //   selectedButton.textContent = currentPage + (i - 2);
+//     //   if (currentPage === currentPage + (i - 2)) {
+//     //     selectedButton.classList.add('active');
+//     //   }
+//     //   const firstButton = document.querySelector('.pagination-button-1');
+//     //   const lastButton = document.querySelector('.pagination-button-7');
+//     //   firstButton.classList.add('hidden');
+//     //   lastButton.textContent = amountOfPages;
+//     // }
+//   };
+//   checkAndRemoveArrows(currentPage, amountOfPages);
+//   checkAndRemoveDots(currentPage, amountOfPages);
+//   setupButtons(currentPage, amountOfPages);
+//   getPaginationButtons();
+
+//   // function dotsAndArrowsRemoval(currentPage, amountOfPages) {}
+
+//   //   const button = document.querySelector('.pagination-button');
+//   //   button.addEventListener('click', handleActivePageNumber(currentPage));
+//   //   console.log(currentPage);
+// }
+
+// // function setupButtons(currentPage, totalPages) {
+// //   let currentButton = document.querySelector(`.pagination-button-4`);
+// //   const lastButton = document.querySelector('.pagination-button-7');
+// //   const rightDots = document.querySelector('#next-dots');
+// //   //Jeśli mniej nic 7 stron to usun niepotrzebne buttony
+// //   if (totalPages < 7) {
+// //     for (let i = 7; i > totalPages; i--) {
+// //       const ithButton = document.querySelector(`.pagination-button-${i}`);
+// //       if (i > totalPages) {
+// //         ithButton.classList.add('hidden');
+// //       }
+// //     }
+// //   }
+
+// //   //szczególne przypadki
+// //   if (currentPage <= 3) {
+// //     currentButton = document.querySelector(`.pagination-button-${currentPage}`);
+// //     currentButton.classList.add('active');
+// //     for (let i = currentPage + 3; i <= 6; i++) {
+// //       const selectedButton = document.querySelector(`.pagination-button-${i}`);
+// //       selectedButton.classList.add('hidden');
+// //     }
+// //     console.log(totalPages, currentPage + 2);
+// //     if (totalPages > currentPage + 2) {
+// //       lastButton.classList.remove('hidden');
+// //       rightDots.classList.remove('hidden');
+// //       lastButton.textContent = totalPages;
+// //       lastButton.dataset.goToPage = totalPages;
+// //     }
+// //   } else if (currentPage >= totalPages - 2 && totalPages >= 7) {
+// //     lastButton.textContent = totalPages;
+// //     lastButton.dataset.goToPage = totalPages;
+
+// //     for (let i = 2; i <= 2 + (totalPages - currentPage); i++) {
+// //       const selectedButton = document.querySelector(`.pagination-button-${i}`);
+// //       selectedButton.classList.add('hidden');
+// //     }
+
+// //     currentButton = document.querySelector(
+// //       `.pagination-button-${
+// //         Math.min(7, totalPages) - (totalPages - currentPage)
+// //       }`
+// //     );
+// //     currentButton.classList.add('active');
+
+// //     for (let i = 2; i <= 6; i++) {
+// //       const selectedButton = document.querySelector(`.pagination-button-${i}`);
+// //       selectedButton.textContent = totalPages - 7 + i;
+// //       selectedButton.dataset.goToPage = totalPages - 7 + i;
+// //     }
+// //   }
+
+// //   if (currentPage > 4 && currentPage < totalPages - 2) {
+// //     lastButton.textContent = totalPages;
+// //     lastButton.dataset.goToPage = totalPages;
+// //     for (let i = 2; i <= 6; i++) {
+// //       const selectedButton = document.querySelector(`.pagination-button-${i}`);
+// //       selectedButton.textContent = currentPage + (i - 4);
+// //       selectedButton.dataset.goToPage = currentPage + (i - 4);
+// //       //   console.log(
+// //       //     'condition chech',
+// //       //     i,
+// //       //     currentPage,
+// //       //     currentPage === currentPage + (i - 4)
+// //       //   );
+// //       if (currentPage === currentPage + (i - 4)) {
+// //         selectedButton.classList.add('active');
+// //       }
+// //     }
+// //   }
+// // }
+
+function checkAndRemoveArrows(currentPage, totalPages) {
+  const leftArrow = document.querySelector('#prev-button');
+  const rightArrow = document.querySelector('#next-button');
+
+  if (currentPage === 1) {
+    leftArrow.classList.add('hidden');
+  }
+  if (currentPage === totalPages) {
+    rightArrow.classList.add('hidden');
+  }
+}
+
+function checkAndRemoveDots(currentPage, totalPages) {
+  const leftDots = document.querySelector('#prev-dots');
+  const rightDots = document.querySelector('#next-dots');
+
+  if (currentPage <= 4) {
+    leftDots.classList.add('hidden');
+  }
+  if (currentPage >= totalPages - 3 || totalPages < 7) {
+    rightDots.classList.add('hidden');
+  }
+}
+
+export function createPagination({ currentPage, totalPages }) {
+  const pagination = document.querySelector('.movies__pagination');
+  const buttons = pagination.querySelectorAll('.pagination-button');
+
+  checkAndRemoveArrows(currentPage, totalPages);
+  checkAndRemoveDots(currentPage, totalPages);
+
+  for (let i = 1; i < buttons.length - 1; i++) {
+    const button = buttons[i];
+
+    if (i === 2 || i === 8) {
+      continue;
+    }
+
+    if (i === 1) {
+      //button 1
+      if (currentPage > 3) {
+        continue;
+      } else if (currentPage <= 3) {
+        button.classList.add('hidden');
+      }
+    }
+    if (i === 3) {
+      //button 2
+      if (currentPage - 2 > 0) {
+        button.textContent = currentPage - 2;
+        button.dataset.goToPage = currentPage - 2;
+      } else {
+        button.classList.add('hidden');
+      }
+    }
+
+    if (i === 4) {
+      //button 3
+      if (currentPage - 1 > 0) {
+        button.textContent = currentPage - 1;
+        button.dataset.goToPage = currentPage - 1;
+      } else {
+        button.classList.add('hidden');
+      }
+    }
+
+    if (i === 5) {
+      //button 4
+      button.textContent = currentPage;
+      button.dataset.goToPage = currentPage;
+    }
+
+    if (i === 6) {
+      //button 5
+      if (currentPage + 1 <= totalPages) {
+        button.textContent = currentPage + 1;
+        button.dataset.goToPage = currentPage + 1;
+      } else {
+        button.classList.add('hidden');
+      }
+    }
+    if (i === 7) {
+      //button 6
+      if (currentPage + 2 <= totalPages) {
+        button.textContent = currentPage + 2;
+        button.dataset.goToPage = currentPage + 2;
+      } else {
+        button.classList.add('hidden');
+      }
+    }
+
+    if (i === 9) {
+      //last page button
+      if (totalPages > currentPage + 2) {
+        button.textContent = totalPages;
+        button.dataset.goToPage = totalPages;
+      } else {
+        button.classList.add('hidden');
+      }
+    }
+
+    if (Number(button.dataset.goToPage) === currentPage) {
+      button.classList.add('active');
+    }
+  }
+}
