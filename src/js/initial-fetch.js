f89447db106b32f8bbe3285b91fe493356e7924f
Jakub Antoniak <antonipomorzanin@gmail.com>
2023-02-20 19:07:03 +0100

    20.02

--- src/js/initial-fetch.js
+++ src/js/initial-fetch.js
@@ -39,11 +39,7 @@ export async function getAndDisplayTrendingMovies(page = 1) {
 
   pagination.removeEventListener('click', pageSelectorClickHandler);
   try {
-<<<<<<< HEAD
-    const trendingMovies = await getTrendingMovies(2);
-=======
     const trendingMovies = await getTrendingMovies(page);
->>>>>>> feature/pagination-v2
 
     const MAIN = document.querySelector('.movies__list');
 
@@ -65,88 +61,3 @@ export async function getAndDisplayTrendingMovies(page = 1) {
 
   Loading.remove();
 }
-<<<<<<< HEAD
-
-//
-//
-
-//
-//
-
-//
-//
-function createPagination({ currentPage, amountOfPages }) {
-  const nextButton = document.querySelector('#next-button');
-  const prevButton = document.querySelector('#prev-button');
-  const paginationNumbers = document.querySelector('.pagination-numbers');
-  const prevDots = document.querySelector('#prev-dots');
-  const nextDots = document.querySelector('#next-dots');
-  const firstButton = document.querySelector('.pagination-button-1');
-  const lastButton = document.querySelector('.pagination-button-7');
-  const currentPageButton = document.querySelector(
-    `.pagination-button-${currentPage}`
-  );
-
-  function paginationButtonCreation(currentPage) {
-    const paginationButton = document.createElement('button');
-    paginationButton.classList.add('pagination-button');
-    paginationButton.classList.add(`pagination-button-${currentPage}`);
-    paginationButton.innerHTML = currentPage;
-
-    paginationNumbers.insertAdjacentElement('beforeend', paginationButton);
-  }
-
-  const firstPageSelected = () => {
-    for (let i = 2; i <= 6; i++) {
-      paginationButtonCreation(i);
-      const selectedButton = document.querySelector(`.pagination-button-${i}`);
-
-      selectedButton.textContent = currentPage + (i - 2);
-
-      if (currentPage === currentPage + (i - 2)) {
-        selectedButton.classList.add('active');
-      }
-      firstButton.classList.add('hidden');
-      lastButton.textContent = amountOfPages;
-    }
-
-    if (currentPage === 1) {
-      prevButton.classList.add('hidden');
-      prevDots.classList.add('hidden');
-    }
-
-    if (currentPage === 2) {
-      prevDots.classList.add('hidden');
-      firstButton.classList.remove('hidden');
-      currentPage === currentPage + (i - 6);
-    }
-
-    if (currentPage === amountOfPages) {
-      nextButton.classList.add('hidden');
-      nextDots.classList.add('hidden');
-    }
-  };
-
-  firstPageSelected();
-
-  // function dotsAndArrowsRemoval(currentPage, amountOfPages) {}
-
-  const button = document.querySelector('.pagination-button');
-  button.addEventListener('click', handleActivePageNumber(currentPage));
-  console.log(currentPage);
-}
-
-//
-
-//
-
-//
-//
-//
-//
-//
-//
-//
-//
-=======
->>>>>>> feature/pagination-v2
