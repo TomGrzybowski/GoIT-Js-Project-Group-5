3b2cbaabe229d0b5b20d31fb138dc20c71e18021
TomGrzybowski <111274544+TomGrzybowski@users.noreply.github.com>
2023-02-21 21:19:50 +0100

    fix!

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './js/search-movies.js';
 import './js/modal-film';
 import './js/modal-team';
 import './js/local-storage';
-import './js/my-library.js';
+// import './js/my-library.js';
 import './js/arrow-up';
 
 window.onload = getAndDisplayTrendingMovies(1);
