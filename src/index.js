a5950ca16fe20c34cf0f1451af35ecaacb933755
anettgor <anetagorowska06@onet.pl>
2023-02-21 21:36:43 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './sass/main.scss';
+import './js/initial-fetch.js';
 import { getAndDisplayTrendingMovies } from './js/initial-fetch.js';
 import './js/search-movies.js';
 import './js/modal-film';
+import './js/modal-team';
 import './js/local-storage';
+// import './js/my-library.js';
+import './js/arrow-up';
 
 window.onload = getAndDisplayTrendingMovies(1);
